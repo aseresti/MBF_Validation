bd23d2267be2ea30a3dec997824fcadd8e41a6d5
aseresti <aseresti@ryerson.ca>
2024-09-17 13:27:35 -0400

    Add vtkImageReader and vtk_to_numpy

--- Ventricle_Segmentation/ImageTools/ConvertVTK2NIFTI.py
+++ Ventricle_Segmentation/ImageTools/ConvertVTK2NIFTI.py
@@ -1,8 +1,9 @@
 import os
 import argparse
 import vtk
-import nibabel
+import nibabel as nib
 import numpy as np
+
 
 class ConvertVTK2NIFTI():
     """Converts vtk image formats (.vti, .vtk) into NIFTI image formats (.nii, .nii.gz)
@@ -10,19 +11,50 @@ class ConvertVTK2NIFTI():
     def __init__(self,Args) -> None:
         self.Args = Args
 
-    def ReadInputImages(self):
-        print(self.Args.InputFolder)
-        self.vtk_Images = []
-        print(os.listdir(self.Args.InputFolder))
-        exit(0)
-        for filename in os.listdir(self.Args.InputFolder):
-            print(filename)
-            if filename.endswith('vti') or filename.endswith('vtk'):
-                print(filename)
-                exit(1)
+    def GetImages(self) -> list:
+        """Gets all of the vtk images within the input directory
 
-    def main():
-        pass
+        Returns:
+            list:a list of str 
+        """
+        vtk_Images = []
+        
+        for filename in os.listdir(self.Args.InputFolder):
+            if filename.endswith('vti') or filename.endswith('vtk'):
+                vtk_Images.append(filename)
+        
+        return vtk_Images
+    
+    def ReadVTKImage(self,path) -> vtk.vtkImageData:
+
+        reader = vtk.vtkImageReader()
+        reader.SetFileName(path)
+        reader.Update()
+        return reader.GetOutput()
+
+    def vtk_to_numpy(self,Image) -> np.array:
+        
+        vtk_data = Image.GetPointData().GetScalars()
+        dims = Image.GetDimensions()
+        numpy_data = vtk.util.numpy_support.vtk_to_numpy(vtk_data)
+        numpy_data = numpy_data.reshape(dims,order='F')
+        
+        return numpy_data
+    
+    def main(self) -> None:
+        
+        vtk_Images = self.GetImages()
+        vtk_Images.sort()
+
+        vtk_dict = dict()
+        for image in vtk_Images:
+            image_path = f"{self.Args.InputFolder}/{image}"
+            Image = self.ReadVTKImage(image_path)
+            print(self.vtk_to_numpy(Image))
+            exit(1)
+            vtk_dict.update({f"{image}":f"{Image}"})
+            
+            
 
 if __name__=="__main__":
     Parser = argparse.ArgumentParser()
@@ -30,4 +62,4 @@ if __name__=="__main__":
     Parser.add_argument("-Nformat", "--Nformat", type= str, default=".nii.gz", dest="Nformat", required= True, help="NIFTI supported file formats: .nii or .nii.gz")
     args = Parser.parse_args()
 
-    ConvertVTK2NIFTI(args).ReadInputImages()
+    ConvertVTK2NIFTI(args).main()
