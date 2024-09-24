04328f4dbfbdb93f42935bf70355b76339b58b6c
aseresti <aseresti@ryerson.ca>
2024-09-24 09:45:25 -0400

    Add label volume and probe filter functions

--- Ventricle_Segmentation/ImageTools/ConvertVTU2NIFTI.py
+++ Ventricle_Segmentation/ImageTools/ConvertVTU2NIFTI.py
@@ -6,20 +6,21 @@ import argparse
 from vtk.util.numpy_support import vtk_to_numpy
 from ConvertVTK2NIFTI import ConvertVTK2NIFTI
 
-class ConvertVTU2NIFTI():
+class ConvertSurface2NIFTI():
     def __init__(self, Args) -> None:
         self.Args = Args
-        _, output_dir = os.path.split(self.Args.InputVTUFile)
+        output_dir, _ = os.path.split(self.Args.InputSurface)
         self.output_file_path = os.path.join(output_dir,self.Args.OutputFileName)
 
-    def VTUReader(self,path):
+    def VTPReader(self,path):
         reader = vtk.vtkXMLPolyDataReader()#UnstructuredGridReader()
         reader.SetFileName(path)
         reader.Update()
         unstructuredgrid = reader.GetOutput()
-        print(unstructuredgrid.GetPointData())
-        print(unstructuredgrid.GetPointData().GetScalars() is None)
+        #print(unstructuredgrid.GetPointData())
+        #print(unstructuredgrid.GetPointData().GetScalars() is None)
 
+        '''
         bounds = unstructuredgrid.GetBounds()
 
         structuredgrid = vtk.vtkImageData()
@@ -42,27 +43,100 @@ class ConvertVTU2NIFTI():
 
         print(probfilter.GetOutput().GetPointData().GetScalars() is None)
         exit(0)
+        '''
+        return unstructuredgrid
+    
+    def probfilter(self, Enclosed, Image):
+        
+        bounds = Image.GetBounds()
 
-        return probfilter.GetOutput()
+        structuredgrid = vtk.vtkImageData()
+        structuredgrid.SetSpacing(0.5,0.5,0.5)
+        structuredgrid.SetOrigin(bounds[0], bounds[2], bounds[4])
 
-    def labelvolume(self, polydata):
-        points = polydata.GetPoints()
-        num_points = points.GetNumberOfPoints()
+        dims = [
+            int((bounds[1]-bounds[0]) / 0.5),
+            int((bounds[3]-bounds[2]) / 0.5),
+            int((bounds[5]-bounds[4]) / 0.5)
+        ]
 
-        labels = np.zeros(num_points, dtype=np.uint8)
-        #todo: add vtkSelectEnclosedPoints()
+        structuredgrid.SetDimensions(dims)
+        
+        #print(dir(structuredgrid.GetPointData()))
+        #exit(0)
+        Image.GetPointData().AddArray(Enclosed)
+        Image.Modified()
+
+        '''
+        probfilter = vtk.vtkProbeFilter()
+        probfilter.SetInputData(structuredgrid)
+        probfilter.SetSourceData(Enclosed)
+        probfilter.Update()
+        '''
+        return Image
+
+    def vtk2numpy(self,Image) -> np.array:
+        """Converts vtk image scalar data into a numpy array with the same dimensions.
+
+        Args:
+            Image (vtk.vtkImageData): The output of ReadVTKImage
+
+        Returns:
+            np.array: numpy array of the image scalars
+        """
+        
+        vtk_data = Image
+        dims = Image.GetDimensions()
+        numpy_data = vtk_to_numpy(vtk_data)
+        numpy_data = numpy_data.reshape(dims,order='F')
+        
+        return numpy_data
+
+
+    def labelvolume(self, Surface, Image):
+        
+        PointsVTK=vtk.vtkPoints()
+        PointsVTK.SetNumberOfPoints(Image.GetNumberOfPoints())
+
+        for i in range(Image.GetNumberOfPoints()):
+            PointsVTK.SetPoint(i,Image.GetPoint(i))
+		
+        print ("--- Converting Image Points into a Polydata")
+		#Convert into a polydata format
+        pdata_points = vtk.vtkPolyData()
+        pdata_points.SetPoints(PointsVTK)
+
+        #labels = np.zeros(num_points, dtype=np.uint8)
+        selectEnclosed = vtk.vtkSelectEnclosedPoints()
+        selectEnclosed.SetInputData(pdata_points) #Points in the Image
+        selectEnclosed.SetSurfaceData(Surface) #Surface Model
+        selectEnclosed.SetTolerance(0.00000000001)
+        selectEnclosed.Update()
+
+        return selectEnclosed.GetOutput().GetPointData().GetArray("SelectedPoints")
+    
+    def WriteVTK(self, Image):
+        writer = vtk.vtkDataSetWriter()#vtkXMLImageDataWriter()
+        writer.SetFileName(self.output_file_path)
+        writer.SetInputData(Image)
+        writer.Write()
 
     def main(self):
-        vtufile = self.VTUReader(self.Args.InputVTUFile)
+        Surface = self.VTPReader(self.Args.InputSurface)
         #todo: add labelvolume()
         class_arguments = argparse.Namespace(InputFolder=None, Nformat=".nii.gzz")
-        np_array = ConvertVTK2NIFTI(class_arguments).vtk2numpy(vtufile)
-        ConvertVTK2NIFTI(class_arguments).numpy2NIFTI(np_array, self.output_file_path)
+        Image = ConvertVTK2NIFTI(class_arguments).ReadVTKImage(self.Args.InputImage)
+        Enclosed = self.labelvolume(Surface, Image)
+        Labeled_Image = self.probfilter(Enclosed, Image)
+        self.WriteVTK(Labeled_Image)
+        #np_array = self.vtk2numpy(Labeled_Image)#ConvertVTK2NIFTI(class_arguments).vtk2numpy(Labeled_Image)
+        #ConvertVTK2NIFTI(class_arguments).numpy2NIFTI(np_array, self.output_file_path)
 
 if __name__=="__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-InputVTUFile", "--InputVTUFile", required=True, dest="InputVTUFile", type=str)
+    parser.add_argument("-InputSurface", "--InputSurface", required=True, dest="InputSurface", type=str)
+    parser.add_argument("-InputImage", "--InputImage", required=True, dest="InputImage", type=str)
     parser.add_argument("-OutputFileName", "--OutputFileName", required=True, dest="OutputFileName", type=str)
     args = parser.parse_args()
 
-    ConvertVTU2NIFTI(args).main()
+    ConvertSurface2NIFTI(args).main()
