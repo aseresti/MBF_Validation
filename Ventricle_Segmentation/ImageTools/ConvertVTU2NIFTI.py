6a87640ec870f31019aa52ccb4fbdcbcd917761a
aseresti <aseresti@ryerson.ca>
2024-09-25 18:29:00 -0400

    Modify Output Image Name

--- Ventricle_Segmentation/ImageTools/ConvertVTU2NIFTI.py
+++ Ventricle_Segmentation/ImageTools/ConvertVTU2NIFTI.py
@@ -1,5 +1,5 @@
 import vtk
-import nibabel as nib
+#import nibabel as nib
 import numpy as np
 import os
 import argparse
@@ -9,42 +9,16 @@ from ConvertVTK2NIFTI import ConvertVTK2NIFTI
 class ConvertSurface2NIFTI():
     def __init__(self, Args) -> None:
         self.Args = Args
-        output_dir, _ = os.path.split(self.Args.InputSurface)
-        self.output_file_path = os.path.join(output_dir,self.Args.OutputFileName)
+        output_dir, InputSurfaceName = os.path.split(self.Args.InputSurface)
+        InputSurfaceName = os.path.splitext(InputSurfaceName)[0]
+        _, ImageName = os.path.split(self.Args.InputImage)
+        self.output_file_path = os.path.join(output_dir,ImageName + "_" +InputSurfaceName)
 
     def VTPReader(self,path):
-        reader = vtk.vtkXMLPolyDataReader()#UnstructuredGridReader()
+        reader = vtk.vtkXMLPolyDataReader()
         reader.SetFileName(path)
         reader.Update()
-        unstructuredgrid = reader.GetOutput()
-        #print(unstructuredgrid.GetPointData())
-        #print(unstructuredgrid.GetPointData().GetScalars() is None)
-
-        '''
-        bounds = unstructuredgrid.GetBounds()
-
-        structuredgrid = vtk.vtkImageData()
-        structuredgrid.SetSpacing(0.5,0.5,0.5)
-        structuredgrid.SetOrigin(bounds[0], bounds[2], bounds[4])
-
-        dims = [
-            int((bounds[1]-bounds[0]) / 0.5),
-            int((bounds[3]-bounds[2]) / 0.5),
-            int((bounds[5]-bounds[4]) / 0.5)
-        ]
-
-        structuredgrid.SetDimensions(dims)
-
-
-        probfilter = vtk.vtkProbeFilter()
-        probfilter.SetInputData(structuredgrid)
-        probfilter.SetSourceData(unstructuredgrid)
-        probfilter.Update()
-
-        print(probfilter.GetOutput().GetPointData().GetScalars() is None)
-        exit(0)
-        '''
-        return unstructuredgrid
+        return reader.GetOutput()
     
     def CreateLabeledImage(self, Enclosed, Image):
         
@@ -74,19 +48,8 @@ class ConvertSurface2NIFTI():
             iZ = int(round((z-origin[2])/spacing[2]))
 
             if 0 <= iX < dims[0] and 0 <= iY < dims[1] and 0 <= iZ < dims[2]:
-                #print(f"Setting scalar at ({iX}, {iY}, {iZ}) to {scalar}")
                 structuredgrid.SetScalarComponentFromFloat(iX,iY,iZ,0,scalar)
 
-
-
-
-
-        '''
-        probfilter = vtk.vtkProbeFilter()
-        probfilter.SetInputData(structuredgrid)
-        probfilter.SetSourceData(Enclosed)
-        probfilter.Update()
-        '''
         return structuredgrid
 
     def vtk2numpy(self,Image) -> np.array:
@@ -127,7 +90,7 @@ class ConvertSurface2NIFTI():
         selectEnclosed = vtk.vtkSelectEnclosedPoints()
         selectEnclosed.SetInputData(pdata_points) #Points in the Image
         selectEnclosed.SetSurfaceData(Surface) #Surface Model
-        selectEnclosed.SetTolerance(0.00000000001)
+        selectEnclosed.SetTolerance(0.000000001)
         selectEnclosed.Update()
 
         #SelectedArray = selectEnclosed.GetOutput().GetPointData().GetArray("SelectedPoints")
@@ -136,26 +99,24 @@ class ConvertSurface2NIFTI():
     
     def WriteVTK(self, Image):
         writer = vtk.vtkDataSetWriter()#vtkXMLImageDataWriter()
-        writer.SetFileName(self.output_file_path)
+        writer.SetFileName(self.output_file_path + ".vtk")
         writer.SetInputData(Image)
         writer.Write()
 
     def main(self):
         Surface = self.VTPReader(self.Args.InputSurface)
-        #todo: add labelvolume()
         class_arguments = argparse.Namespace(InputFolder=None, Nformat=".nii.gzz")
         Image = ConvertVTK2NIFTI(class_arguments).ReadVTKImage(self.Args.InputImage)
         Enclosed = self.LabelEnclosedPoints(Surface, Image)
         Labeled_Image = self.CreateLabeledImage(Enclosed, Image)
         self.WriteVTK(Labeled_Image)
         #np_array = self.vtk2numpy(Labeled_Image)#ConvertVTK2NIFTI(class_arguments).vtk2numpy(Labeled_Image)
-        #ConvertVTK2NIFTI(class_arguments).numpy2NIFTI(np_array, self.output_file_path)
+        #ConvertVTK2NIFTI(class_arguments).numpy2NIFTI(np_array, self.output_file_path + ".nii.gz")
 
 if __name__=="__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-InputSurface", "--InputSurface", required=True, dest="InputSurface", type=str)
     parser.add_argument("-InputImage", "--InputImage", required=True, dest="InputImage", type=str)
-    parser.add_argument("-OutputFileName", "--OutputFileName", required=True, dest="OutputFileName", type=str)
     args = parser.parse_args()
 
     ConvertSurface2NIFTI(args).main()
