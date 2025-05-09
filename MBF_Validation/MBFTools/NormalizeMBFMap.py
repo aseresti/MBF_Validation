bd71e9ad68803ad0555064bd37485694e79da2e0
aseresti <aseresti@ryerson.ca>
2025-05-09 12:16:51 -0400

    Add Volume calculations in selected territories

--- MBF_Validation/MBFTools/NormalizeMBFMap.py
+++ MBF_Validation/MBFTools/NormalizeMBFMap.py
@@ -1,13 +1,20 @@
 import os
+import vtk
 import numpy as np
 import argparse
-from utilities import ReadVTUFile, WriteVTUFile
+from utilities import ReadVTUFile, WriteVTUFile, ThresholdInBetween, ExtractSurface
 from vtk.util.numpy_support import vtk_to_numpy, numpy_to_vtk
 
 class MBFNormalization():
     def __init__(self, args):
         self.args = args
         self.MBF = ReadVTUFile(self.args.InputMBFMap)
+        labels = f"{os.path.splitext(self.args.InputMBFMap)[0]}_Labels.dat"
+        self.Labels = {}
+        with open(labels, "r") as ifile:
+            for LINE in ifile:
+                line = LINE.split()
+                self.Labels[line[1]] = line[0]
 
     def Normalize(self):
         ImageScalars = self.MBF.GetPointData().GetArray(self.args.ArrayName)
@@ -18,10 +25,46 @@ class MBFNormalization():
         OPath = f"{os.path.splitext(self.args.InputMBFMap)[0]}_Normalized.vtu"
         WriteVTUFile(OPath, self.MBF)
 
+    def CalculateVolume(self,ClosedSurface):
+        tri_filter = vtk.vtkTriangleFilter()
+        tri_filter.SetInputData(ClosedSurface)
+        tri_filter.Update()
+
+        cleaner = vtk.vtkCleanPolyData()
+        cleaner.SetInputData(tri_filter.GetOutput())
+        cleaner.Update()
+
+        Mass = vtk.vtkMassProperties()
+        Mass.SetInputData(cleaner.GetOutput())
+        Mass.Update()
+
+        return Mass.GetVolume()
+    
+    def CollectTerritoryInfo(self):
+        Volume = 0
+        MBF_sum = 0
+        for tag in self.args.TerritoryTag:
+            for (key, item) in self.Labels:
+                if tag in key:
+                    territory_ = ThresholdInBetween(self.MBF, "TerritoryMaps", item, item)
+                    Volume += self.CalculateVolume(ExtractSurface(territory_))
+                    index_mbf_ = vtk_to_numpy(territory_.GetPointData().GetArray("IndexMBF"))
+                    MBF_sum += np.sum(index_mbf_)
+        return Volume, MBF_sum
+    
+    def main(self):
+        self.Normalize()
+        Volume, MBF_sum = self.CollectTerritoryInfo()
+        print(Volume * MBF_sum)
+        
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-InputMBFMap", "--InputMBFMap", dest = "InputMBFMap", type = str, required = True)
+    parser.add_argument("-InputLabel", "--InputLabel", dest = "InputLabel", type= str, required= True)
     parser.add_argument("-ArrayName", "--ArrayName", dest = "ArrayName", type = str, required = False, default = "ImageScalars")
+    parser.add_argument("-TerritoryTag", "--TerritoryTag", type= str, nargs="+", required=True, dest = "TerritoryTag")
     args = parser.parse_args()
     
     MBFNormalization(args).Normalize()
